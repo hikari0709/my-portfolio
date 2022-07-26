847264768a91c8ef74268d3e8b594a0dc92ae66f
hikari <tanimoto0709@gmail.com>
2022-07-26 21:58:42 +0900

    add: emotionの追加

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,12 @@
+import { css } from '@emotion/react';
+
+const style = css`
+  background-color: #000;
+`;
+
 export const Header = () => {
   return (
-    <header>
+    <header css={style}>
       ここはheaderです。
     </header>
   )
